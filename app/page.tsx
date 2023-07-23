939fd97f6f65543ee1714095f8999c9694384afc
Liam Cotton <37960435+liamcco@users.noreply.github.com>
2023-07-23 19:11:45 +0200

    nice links to stones

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,40 @@
 import { getStones } from "@/sanity/sanity-utils";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function Home() {
   const stones = await getStones();
 
   return (
-    <div>
-      {stones.map((stone) => (
-        <div key={stone._id}>
-          <h2>{stone.name}</h2>
-          <p>{stone.description}</p>
-        </div>
-      ))}
+    <div className="max-w-5xl mx-auto">
+      <h1 className="text-7xl font-extrabold">
+        <span className="bg-gradient-to-r from-green-600 to-green-400 bg-clip-text text-transparent">
+          Ananda
+        </span>{" "}
+        Stones
+      </h1>
+      <p className="mt-3 text-xl text-gray-600">Hitta din sten</p>
+      <h2 className="mt-24 font-bold text-gray-700 text-3xl">Alla stenar</h2>
+      <div className="mt-5 grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {stones.map((stone) => (
+          <Link
+            key={stone._id}
+            href={`/stones/${stone.slug}`}
+            className="border-2 border-gray-500 rounded-lg p-1 hover:scale-105 hover:border-green-500 transition"
+          >
+            {stone.image && (
+              <Image
+                src={stone.image}
+                alt={stone.name}
+                width={750}
+                height={300}
+                className="object-cover rounded-lg border border-gray-500"
+              />
+            )}
+            <div className="font-extrabold mt-2">{stone.name}</div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
