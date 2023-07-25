98ac5fadd2fb81ff23836649c1938ace18ae3a67
Liam Cotton <37960435+liamcco@users.noreply.github.com>
2023-07-25 23:00:29 +0200

    added progress indicator

--- components/StoneGallery.tsx
+++ components/StoneGallery.tsx
@@ -2,18 +2,36 @@
 
 import { Stone } from "@/types/Stone";
 import StoneGalleryCard from "./StoneGalleryCard";
+import { useState } from "react";
 
 interface Props {
   stones: Stone[];
 }
 
 export default function StoneGallery(props: Props) {
+  const [indexOfCurrentStone, setIndexOfCurrentStone] = useState(0);
+
+  function handleScroll(event: React.UIEvent<HTMLDivElement>) {
+    const target = event.target as HTMLDivElement;
+    const scrollLeft = target.scrollLeft;
+    const scrollWidth = target.scrollWidth;
+    const scrollPercentage = scrollLeft / scrollWidth;
+    const index = Math.round(scrollPercentage * props.stones.length);
+    index !== indexOfCurrentStone && setIndexOfCurrentStone(index);
+  }
+
   return (
-    <div className="flex justify-center">
-      <div className="flex w-[24rem] overflow-x-auto snap-x snap-mandatory">
+    <div className="flex flex-col items-center gap-2">
+      <h2 className="text-base text-gray-400 text-center">
+        {indexOfCurrentStone + 1}/{props.stones.length}
+      </h2>
+      <div
+        className="flex w-[24rem] overflow-x-auto snap-x snap-mandatory"
+        onScroll={handleScroll}
+      >
         {props.stones.map((stone) => (
           <div key={stone._id}>
-            <div className="w-[20rem] snap-always snap-center m-[2rem]">
+            <div className="w-[20rem] snap-always snap-center mx-[2rem]">
               <StoneGalleryCard key={stone._id} stone={stone} />
             </div>
           </div>
