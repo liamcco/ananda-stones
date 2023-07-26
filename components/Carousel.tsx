20ece567bda37f253504859202b3da38ed4b7992
Liam Cotton <37960435+liamcco@users.noreply.github.com>
2023-07-26 21:14:27 +0200

    Added buttons to wide-screen carousel

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -20,6 +20,7 @@ export default function Carousel(props: Props) {
     const scrollPercentage = target.scrollLeft / target.scrollWidth;
     const index = Math.round(scrollPercentage * children.length);
     if (index !== indexOfCurrentChild) {
+      console.log("new index: " + index);
       setIndexOfCurrentChild(index);
       props.onNewIndex && props.onNewIndex(index);
     }
@@ -30,20 +31,83 @@ export default function Carousel(props: Props) {
   });
 
   return (
-    <div
-      className="flex w-[24rem] md:w-[48rem] overflow-x-auto snap-x snap-mandatory"
-      onScroll={handleScroll}
-    >
-      {children.map((child, index) => (
-        <div
-          key={child.key}
-          id={props.id ? props.id + index.toString() : undefined}
-        >
-          <div className="w-[20rem] md:w-[40rem] snap-always snap-center mx-[2rem] md:mx-[4rem]">
-            {child}
+    <>
+      <div
+        className="flex w-[24rem] md:w-[48rem] overflow-x-auto snap-x snap-mandatory"
+        onScroll={handleScroll}
+      >
+        {children.map((child, index) => (
+          <div
+            key={child.key}
+            id={props.id ? props.id + index.toString() : undefined}
+          >
+            <div className="w-[20rem] md:w-[40rem] snap-always snap-center mx-[2rem] md:mx-[4rem]">
+              {child}
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+
+      <div className="hidden md:flex justify-between absolute z-10 w-[42rem]">
+        <button
+          className="bg-[#232323] text-[#659cef] rounded-full w-10 h-10 flex justify-center items-center"
+          onClick={() => {
+            if (indexOfCurrentChild > 0) {
+              const cardToFind = "card-" + (indexOfCurrentChild - 1).toString();
+              const card = document.getElementById(cardToFind);
+              if (card) {
+                card.scrollIntoView({
+                  behavior: "smooth",
+                  block: "nearest",
+                });
+              }
+            }
+          }}
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="3"
+              d="M15 19l-7-7 7-7"
+            ></path>
+          </svg>
+        </button>
+        <button
+          className="bg-[#232323] text-[#659cef] rounded-full w-10 h-10 flex justify-center items-center"
+          onClick={() => {
+            if (indexOfCurrentChild < children.length - 1) {
+              const cardToFind = "card-" + (indexOfCurrentChild + 1).toString();
+              const card = document.getElementById(cardToFind);
+              if (card) {
+                card.scrollIntoView({
+                  behavior: "smooth",
+                  block: "nearest",
+                });
+              }
+            }
+          }}
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="3"
+              d="M9 5l7 7-7 7"
+            ></path>
+          </svg>
+        </button>
+      </div>
+    </>
   );
 }
