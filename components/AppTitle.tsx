c5643eb8f7b979ee16c86c6e343a5f4cf5f17c19
Liam Cotton <37960435+liamcco@users.noreply.github.com>
2023-07-29 11:21:57 +0200

    Better logo styling

--- components/AppTitle.tsx
+++ components/AppTitle.tsx
@@ -3,10 +3,12 @@ import Logo from "./shared/Logo";
 export default function AppTitle() {
   return (
     <div className="m-4 flex justify-center items-center">
-      <Logo />
-      <h1 className="text-4xl md:text-7xl font-extrabold p-2 m-4 w-fit">
+      <div className="w-24">
+        <Logo />
+      </div>
+      <span className="text-4xl md:text-7xl font-extrabold p-2 m-4">
         Ananda Stones
-      </h1>
+      </span>
     </div>
   );
 }
--- components/shared/Logo.tsx
+++ components/shared/Logo.tsx
@@ -3,12 +3,8 @@ import Image from "next/image";
 
 export default function Logo() {
   return (
-    <div className="bg-white rounded-full max-w-[6rem] max-h-24 shadow-md">
-      <Image
-        src={logo}
-        alt="Ananda Garden Logo"
-        className="h-full w-full p-4"
-      />
+    <div className="bg-white rounded-full shadow-md aspect-square flex justify-center items-center">
+      <Image src={logo} alt="Ananda Garden Logo" className="w-3/4" />
     </div>
   );
 }
