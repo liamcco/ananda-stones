1fd38fbe70ec43643b28df26bafcba739906da91
Liam Cotton <37960435+liamcco@users.noreply.github.com>
2023-07-29 13:45:02 +0200

    fix logo

--- components/AppTitle.tsx
+++ components/AppTitle.tsx
@@ -3,10 +3,10 @@ import Logo from "./shared/Logo";
 export default function AppTitle() {
   return (
     <div className="m-4 flex justify-center items-center">
-      <div className="w-24">
+      <div className="w-[212px]">
         <Logo />
       </div>
-      <span className="text-4xl md:text-7xl font-extrabold p-2 m-4">
+      <span className="text-4xl text-right md:text-7xl font-extrabold p-2 m-4">
         Ananda Stones
       </span>
     </div>
