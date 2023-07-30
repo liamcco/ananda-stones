f24d7425ac10e90a0e0b45c02350cba045a40c40
Liam Cotton <37960435+liamcco@users.noreply.github.com>
2023-07-30 18:08:36 +0200

    moved sanity admin panel to seperate repo

--- sanity/schemas/stonePropertiesData/index.ts
+++
@@ -1,5 +0,0 @@
-export { zodiacEntries } from "./zodiacEntries";
-export { elementEntries } from "./elementEntries";
-export { tagEntries } from "./tagEntries";
-export { chakraEntries } from "./chakraEntries";
-export { monthEntries } from "./monthEntries";
